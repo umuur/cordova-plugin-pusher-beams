1ebc3e11360d387738501a89a881a0495443bf04
Umuur <umur.dincer@#gmail.com>
2022-03-29 17:41:39 +0000

    fix instance

--- src/ios/AppDelegate+PusherBeams.h
+++ src/ios/AppDelegate+PusherBeams.h
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate (PusherBeams) <UIApplicationDelegate>
-
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
 @end
